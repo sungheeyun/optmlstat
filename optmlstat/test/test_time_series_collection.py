d668ddf38959b02279fcb3344d5d79c0bae24f99
Sunghee Yun <sunghee.yun@gmail.com>
2020-11-07 23:49:08 +0900

    Added name attribute to optmlstat.time_series.time_series_collection.TimeSeriesCollection

---
+++ optmlstat/test/test_time_series_collection.py
@@ -0,0 +1,39 @@
+import unittest
+import sys
+
+from pandas import DataFrame, read_csv, Timestamp
+
+from optmlstat.time_series.time_series import TimeSeries
+from optmlstat.time_series.time_series_collection import TimeSeriesCollection
+
+if sys.version_info[0] < 3:
+    from StringIO import StringIO
+else:
+    from io import StringIO
+
+
+TIME_SERIES_DATA_TEXT: StringIO = StringIO(
+    """time;value
+    2020-1-1 00:01:00;10
+    2020-1-1 00:02:00;20
+    2020-1-1 00:03:00;30
+    2020-1-1 00:06:00;40
+    """
+)
+
+
+class TestTimeSeriesCollection(unittest.TestCase):
+    def test_time_series_collection(self):
+        data_frame: DataFrame = read_csv(TIME_SERIES_DATA_TEXT, sep=";", index_col=0)
+        data_frame.index = data_frame.index.map(Timestamp)
+
+        time_series: TimeSeries = TimeSeries(data_frame)
+
+        time_series_collection: TimeSeriesCollection = TimeSeriesCollection([time_series] * 5)
+
+        for time_series in time_series_collection.time_series_list:
+            self.assertEqual(time_series.name, "time_series_0")
+
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ optmlstat/time_series/time_series_collection.py
@@ -0,0 +1,13 @@
+from typing import List
+
+from optmlstat.basic_modules.class_base import OptMLStatClassBase
+from optmlstat.time_series.time_series import TimeSeries
+
+
+class TimeSeriesCollection(OptMLStatClassBase):
+    """
+    Collection of TimeSeries instances.
+    """
+
+    def __init__(self, time_series_list: List[TimeSeries]) -> None:
+        self.time_series_list: List[TimeSeries] = time_series_list.copy()
